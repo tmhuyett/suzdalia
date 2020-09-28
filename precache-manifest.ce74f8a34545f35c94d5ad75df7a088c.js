c3d45abaa4dfe68be1a489922af9c6a8cc35f21f
tmhuyett <toddhuyett@gmail.com>
2020-09-28 09:52:40 -0400

    Updates

---
+++ precache-manifest.ce74f8a34545f35c94d5ad75df7a088c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300aec90a88391e8d4fd34dbbccdf87f",
+    "url": "/tmhuyett/suzdalia.git/index.html"
+  },
+  {
+    "revision": "996577bf860a7dd99b82",
+    "url": "/tmhuyett/suzdalia.git/static/css/main.62927a30.chunk.css"
+  },
+  {
+    "revision": "cdbb2007aac207594742",
+    "url": "/tmhuyett/suzdalia.git/static/js/2.32710e35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tmhuyett/suzdalia.git/static/js/2.32710e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996577bf860a7dd99b82",
+    "url": "/tmhuyett/suzdalia.git/static/js/main.37fe0722.chunk.js"
+  },
+  {
+    "revision": "72cd0e8d13d6e50891e4",
+    "url": "/tmhuyett/suzdalia.git/static/js/runtime-main.46e462c7.js"
+  }
+]);
